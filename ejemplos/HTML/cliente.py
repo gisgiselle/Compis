a639a739d1e7acaff6939a237d8aa8506a197bf8
Javier Abdul Córdoba Gándara <abdul@tec.mx>
2022-09-20 21:52:16 -0500

    Ejemplo HTML

---
+++ ejemplos/HTML/cliente.py
@@ -0,0 +1,23 @@
+from antlr4 import *
+from antlr.HTMLParserListener import HTMLParserListener
+from antlr.HTMLLexer import HTMLLexer
+from antlr.HTMLParser import HTMLParser
+import sys
+
+
+class TreePrinter(HTMLParserListener):
+    def enterName(self, ctx:HTMLParser.NameContext):
+        print(ctx.TAG_NAME(0).getText())
+
+def main(argv):
+    parser = HTMLParser(CommonTokenStream(HTMLLexer(FileStream("test.html"))))
+    tree = parser.htmlDocument()
+
+    print(tree)
+
+    walker = ParseTreeWalker()
+    walker.walk(TreePrinter(), tree)
+
+
+if __name__ == '__main__':
+    main("test.html")
