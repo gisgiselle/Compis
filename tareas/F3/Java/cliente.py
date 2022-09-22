be127edc00cf69f096be3c1230690e23f8734fd8
Javier Abdul Córdoba Gándara <abdul@tec.mx>
2022-09-22 17:37:41 -0500

    Tarea F3

---
+++ tareas/F3/Java/cliente.py
@@ -0,0 +1,17 @@
+# En esta tarea comenzaremos a trabajar con el API de ANTLR para Python3. Toma como modelo el ejemplo de HTML.
+
+# 1. Descarga la gramática de Java del repositorio público de ANTLR, son dos archivos y guárdalos en el
+#    directorio antlr
+#    https://github.com/antlr/grammars-v4
+# 2. Configura la generación de código.
+# 3. Prueba un programa sencillo de Java usando el ANTLR preview.
+
+# 4. Resuelve las siguientes tareas:
+#     1. Imprimir los nombres de todas las clases
+#     2. Imprimir los nombres y tipos de todos los métodos
+#     3. Imprimir todos los strings
+
+# Puedes hacer 1 solo listener e ir guardando en una lista lo que vayas encontrando, para al final imprimirlo,
+# o bien hacer 3 listeners y mandarlos llamar uno tras otro con el objeto walker.
+
+
